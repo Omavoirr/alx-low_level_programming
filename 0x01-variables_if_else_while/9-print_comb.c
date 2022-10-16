5bde7793e9b709d9d8270b20a85df5b7287ba7d5
Omavoirr <okekeprincessifeoma@yahoo.com>
2022-10-16 20:42:22 +0000

    C programming Variables

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,7 +2,7 @@
 
 /**
  * main - prints all combination of single digits
- * Return: Always
+ * Return: Always 0
  */
 int main(void)
 {
@@ -14,9 +14,9 @@ int main(void)
 		if (n != 57)
 		{
 			putchar(',');
-				putchar(' ');
+			putchar(' ');
 		}
-		putchar('\n');
-		return (0);
+	putchar('\n');
+	return (0);
 	}
 }
