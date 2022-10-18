13fda1ae2a0c408a02867edcd99d6bcd4af1e500
Omavoirr <okekeprincessifeoma@yahoo.com>
2022-10-18 21:53:36 +0000

    Functions and nested loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,4 +1,4 @@
-#include "main"
+#include "main.h"
 
 /**
  * print_alphabet - prints the alpha in lowercase
@@ -10,5 +10,5 @@ void print_alphabet(void)
 	for (la = 'a'; la <= 'z'; la++)
 		_putchar(la);
 
-	putchar('\n');
+	_putchar('\n');
 }
