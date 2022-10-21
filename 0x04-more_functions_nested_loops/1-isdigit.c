7ecee2098bb525a5533a2db32961bdfa229f7c4e
Omavoirr <okekeprincessifeoma@yahoo.com>
2022-10-21 02:29:58 +0000

    more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _is digit - checks if a char is a dgit
+ * @c: int to be checked
+ * Return: 1 if v is a digit, 0 otherwise
+ */
+int _isdigit(int c);
+{
+	int y = 0;
+
+	if (c >= '9' && c <= '9')
+		y = 1;
+
+	return (y);
+}
+
