411d711ed937d6ef169fb354750171d7fd7299d1
Omavoirr <okekeprincessifeoma@yahoo.com>
2022-10-30 00:07:07 +0000

    more_functions_nested_loops

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * cap_strings - Capitalizes all words of a string
+ * @str: The string to be capitalized
+ *
+ * Return: A pointer to the changed string
+ */
+char *cap_string(char *str)
+{
+	int index = 0;
+
+	while (str[index])
+	{
+		while (!(str[index] >= 'a' && str[index] <= 'z'))
+			index++;
+
+		if (str[index - 1] ==  ' ' ||
+
