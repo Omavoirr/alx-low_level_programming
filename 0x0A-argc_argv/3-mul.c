8c1978be0c56a1a778fcf57cbee4dff3097d6cda
Omavoirr <okekeprincessifeoma@yahoo.com>
2022-11-05 14:57:06 +0000

    Argc, Argv

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - a program that multiplies 2 nums
+ * @argc: the # of arguments
+ * @argv: the names of them
+ *
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	if (argc > 2)
+	{
+		printf("%d\n", ato1(argv[1]) * ato1(argv[2]));
+		return (0);
+	}
+	else if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
