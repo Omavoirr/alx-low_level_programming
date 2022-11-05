baab622acb6b32fa12fc94c74cfbc2900455a33e
Omavoirr <okekeprincessifeoma@yahoo.com>
2022-11-05 18:04:25 +0000

    Recursions in C Programming

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -9,8 +9,8 @@
   * factorial - Returns the factorial of a given number.
   * @n: The number to find the factorial of.
   *
-  * Return: if n > 0 = the factorial of n.
-  * if n < 0 = 1 to indicate an error.
+  * Return: if n > 0 - the factorial of n.
+  * if n < 0 - 1 to indicate an error.
   */
 int factorial(int n)
 {
