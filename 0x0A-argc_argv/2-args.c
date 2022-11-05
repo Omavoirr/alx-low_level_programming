ad23109174a1a6d50d5f640ff869562166a7f1e5
Omavoirr <okekeprincessifeoma@yahoo.com>
2022-11-05 15:05:02 +0000

    Argc, Argv

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -7,14 +7,13 @@
  * Return: 0
  *
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	int i = 0;
+	int i;
 
-	while (argc--)
+	for (i = 0; i < argc; i++)
 	{
-		printf("%s\n", argv[1]);
-		i++;
+		printf("%s\n", argv[i]);
 	}
 	return (0);
 }
