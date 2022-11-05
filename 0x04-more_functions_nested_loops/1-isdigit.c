512309f33402f91c2a763e67a071ab8dc4b1eea6
Omavoirr <okekeprincessifeoma@yahoo.com>
2022-11-05 21:00:14 +0000

    More functions nested loops

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * _is digit - checks if a char is a digit
- * @c: int to be checked
- * Return: 1 if c is a digit, 0 otherwise
- */
+  * _isdigit - checks if a char is a digit
+  * @c: int to be checked
+  * Return: 1 if c is a digit, 0 otherwise
+  */
 int _isdigit(int c)
 {
 	int y = 0;
